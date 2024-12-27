9f06167f0ad4e5f0817a22f92f55d00dd503895c
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2024-12-28 02:04:20 +0530

    complete add patient form:

--- frontend/src/components/FormComponent.jsx
+++ frontend/src/components/FormComponent.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { z } from "zod";
 
 import { toast } from "@/hooks/use-toast";
@@ -15,6 +15,14 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 
 const FormSchema = z.object({
@@ -24,16 +32,21 @@ const FormSchema = z.object({
   age: z.number().min(0, {
     message: "Age must be a positive number.",
   }),
-  gender: z.enum(["Male", "Female", "Other"], {
+  gender: z.enum(["male", "female", "other"], {
     message: "Gender must be one of Male, Female, or Other.",
   }),
   address: z.string().min(5, {
     message: "Address must be at least 5 characters.",
   }),
-  contact: z.string().regex(/^\d{10}$/, {
-    message: "Contact number must be exactly 10 digits.",
-  }),
-  images: z.array(z.instanceof(File)).nonempty({
+  occupation: z.string().min(1, { message: "Occupation is required." }),
+  contact: z.preprocess(
+    (val) => String(val), // Preprocess the value to ensure it's a string
+    z
+      .string()
+      .length(10, { message: "Contact number must be exactly 10 digits." })
+      .transform((val) => Number(val)) // process it back to number
+  ),
+  images: z.array(z.instanceof(File)).min(1, {
     message: "At least one image is required.",
   }),
 });
@@ -57,38 +70,125 @@ const FormComponent = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full mx-3 space-y-6"
+      >
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="pathak" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="case-id"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Case Id</FormLabel>
+                <FormControl>
+                  <Input placeholder="pathak" {...field} value="123" disabled />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {/* Age Field */}
+          <FormField
+            control={form.control}
+            name="age"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Age</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="35"
+                    {...field}
+                    type="number"
+                    value={field.value || ""}
+                    onChange={(e) => field.onChange(Number(e.target.value))}
+                    className="no-spinner"
+                    onKeyDown={(e) => {
+                      if (
+                        e.key === "ArrowUp" ||
+                        e.key === "ArrowDown" ||
+                        e.key === "-" ||
+                        e.key === "e" ||
+                        e.key === "E"
+                      ) {
+                        e.preventDefault();
+                      }
+                    }}
+                    onWheel={(e) => e.preventDefault()}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Gender Field */}
+          <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Gender</FormLabel>
+                <FormControl>
+                  <Select
+                    {...field}
+                    value={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select Gender" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="male">Male</SelectItem>
+                      <SelectItem value="female">Female</SelectItem>
+                      <SelectItem value="other">Other</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
         <FormField
           control={form.control}
-          name="name"
+          name="contact"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="pathak" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="age"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Age</FormLabel>
+              <FormLabel>Contact Number</FormLabel>
               <FormControl>
                 <Input
-                  placeholder="35"
+                  placeholder="1234567890"
                   {...field}
-                  type="number"
+                  maxLength={10}
+                  type="tel"
                   value={field.value || ""}
                   onChange={(e) => field.onChange(Number(e.target.value))}
-                  className="no-spinner"
                   onKeyDown={(e) => {
                     if (
                       e.key === "ArrowUp" ||
-                      e.key === "ArrowDown"
+                      e.key === "ArrowDown" ||
+                      e.key === "-" ||
+                      e.key === "e" ||
+                      e.key === "E"
                     ) {
                       e.preventDefault();
                     }
@@ -96,19 +196,66 @@ const FormComponent = () => {
                   onWheel={(e) => e.preventDefault()}
                 />
               </FormControl>
+              <FormDescription>Do not mention country code.</FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your address" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="occupation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Occupation</FormLabel>
+                <FormControl>
+                  <Input placeholder="Occupation" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
 
         <FormField
           control={form.control}
-          name="gender"
+          name="images"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Gender</FormLabel>
+              <FormLabel>Images</FormLabel>
               <FormControl>
-                <Input placeholder="pathak" {...field} />
+                <Controller
+                  control={form.control}
+                  name="images"
+                  render={({ field: { onChange, value } }) => (
+                    <Input
+                      type="file"
+                      multiple // Allows multiple files to be selected
+                      accept="image/*" // Optional: restrict file types to images
+                      onChange={(e) => {
+                        const files = e.target.files;
+                        if (files) {
+                          onChange(Array.from(files)); // Convert FileList to an array
+                        }
+                      }}
+                      value={undefined} // Prevent React warning about controlled input
+                    />
+                  )}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
